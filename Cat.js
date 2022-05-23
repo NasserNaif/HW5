f2b3eeb734101882228fa179e1db9fbfc8316ae2
Nasser Naif <96125347+NasserNaif@users.noreply.github.com>
2022-05-23 06:28:22 +0300

    Add files via upload

---
+++ Cat.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function CatInfo() {
+  return (
+    <div className="GenralDiv">
+      <div className="SmallDiv">
+        <img src="https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_360/public/Abyssinian_body_7.jpg?itok=5g6wdh0S" />
+        <h1>Abyssinian Cat</h1>
+        <h4>Color :</h4>
+        <h5>Ruddy, red, blue, fawn</h5>
+      </div>
+
+      <div className="SmallDiv">
+        <img src="https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_360/public/AmericanBobtail_body_6.jpg?itok=nXSRswr-" />
+        <h1>American Bobtail Cat Breed</h1>
+        <h4>Color :</h4>
+        <h5>
+          White, black, blue, red, cream, chocolate, lavender, cinnamon, fawn,
+          plus various patterns and shadings
+        </h5>
+      </div>
+
+      <div className="SmallDiv">
+        <img src="https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_360/public/Bengal_body_6.jpg?itok=YmBoKW_Z" />
+        <h1>Bengal Cat</h1>
+        <h4>Color :</h4>
+
+        <h5>
+          Bright orange to light brown, with dark spots or a distinctive
+          marbling pattern
+        </h5>
+      </div>
+
+      <div className="SmallDiv">
+        <img src="https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_360/public/Bombay_body_6.jpg?itok=AaO2i3Aj" />
+        <h1>Bombay Cat</h1>
+        <h4>Color :</h4>
+        <h5>Black</h5>
+      </div>
+    </div>
+  );
+}
